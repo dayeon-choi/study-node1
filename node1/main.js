582dedffa0de721e71ba116f0716316d79b2988a
dayeon <dayeon2399@naver.com>
2021-03-11 10:29:38 +0900

    first commit

---
+++ node1/main.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const app = http.createServer(function(req,res){
+    res.writeHead(200);
+    console.log(__dirname+req.url);
+    res.end("hello egoing");
+});
+app.listen(3000);
