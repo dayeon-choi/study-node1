e38cff13069bfa2e6b2e27760cd47b0be43b7eac
dayeon <dayeon2399@naver.com>
2021-03-17 10:01:20 +0900

    Update main.js

--- node1/main.js
+++ node1/main.js
@@ -1,8 +1,22 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const port = 5000;
 
-const app = http.createServer(function(req,res){
+const app = http.createServer((req, res) => {
+    let _url = req.url;
+    const queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+
+    if (_url === "/") {
+        _url = "/index.html";
+    }
+    if (_url === "/favicon.ico") {
+        return res.writeHead(404);
+    }
+
     res.writeHead(200);
-    console.log(__dirname+req.url);
-    res.end("hello egoing");
+    res.end(fs.readFileSync(__dirname + _url));
 });
-app.listen(3000);
+
+app.listen(port);
