20899ff3656863518cab76632ddde075c4c58b5d
dayeon <dayeon2399@naver.com>
2021-03-25 09:51:56 +0900

    Update main.js - 글목록

--- node1/main.js
+++ node1/main.js
@@ -1,22 +1,73 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const port = 5000;
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
 
-const app = http.createServer((req, res) => {
-    let _url = req.url;
-    const queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
+const app = http.createServer(function (request, response) {
+    const _url = request.url
+    const queryData = url.parse(_url, true).query
+    const pathname = url.parse(_url, true).pathname
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            const title = 'Welcome'
+            const description = 'Hello, Node.js'
 
-    if (_url === "/") {
-        _url = "/index.html";
+            fs.readdir('data/', function (err, data) {
+                let list = '<ul>'
+                for (let i = 0; i < data.length; i++) {
+                    list += `<li><a href="/?id=${data[i]}"> ${data[i]} </a></li>`
+                }
+                list += '</ul>'
+                const template = `
+          <!doctype html>
+          <html lang="ko">
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `
+                response.writeHead(200)
+                response.end(template)
+            })
+
+        } else {
+            fs.readdir('data/', function (err, data) {
+                let list = '<ul>'
+                for (let i = 0; i < data.length; i++) {
+                    list += `<li><a href="/?id=${data[i]}"> ${data[i]} </a></li>`
+                }
+                list += '</ul>'
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                    const title = queryData.id
+                    const template = `
+          <!doctype html>
+          <html lang="ko">
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `
+                    response.writeHead(200)
+                    response.end(template)
+                })
+            })
+        }
+    } else {
+        response.writeHead(404)
+        response.end('Not found')
     }
-    if (_url === "/favicon.ico") {
-        return res.writeHead(404);
-    }
-
-    res.writeHead(200);
-    res.end(fs.readFileSync(__dirname + _url));
-});
-
-app.listen(port);
+})
+app.listen(3000)
