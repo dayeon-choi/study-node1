23fd4860b3895c98dd5569eafa4c94a3a3da184d
dayeon <dayeon2399@naver.com>
2021-04-01 09:46:06 +0900

    web2-38 글삭제

--- node1/main.js
+++ node1/main.js
@@ -51,11 +51,16 @@ var app = http.createServer(function(request,response){
 		} else {
 			fs.readdir('./data', function(error, filelist){
 				fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-					const title = queryData.id;
-					const list = templateList(filelist);
-					const template = templateHTML(title, list,
+					var title = queryData.id;
+					var list = templateList(filelist);
+					var template = templateHTML(title, list,
 						`<h2>${title}</h2>${description}`,
-						`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+						` <a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>`
 					);
 					response.writeHead(200);
 					response.end(template);
@@ -64,9 +69,9 @@ var app = http.createServer(function(request,response){
 		}
 	} else if(pathname === '/create'){
 		fs.readdir('./data', function(error, filelist){
-			const title = 'WEB - create';
-			const list = templateList(filelist);
-			const template = templateHTML(title, list, `
+			var title = 'WEB - create';
+			var list = templateList(filelist);
+			var template = templateHTML(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -97,9 +102,9 @@ var app = http.createServer(function(request,response){
 	} else if(pathname === '/update'){
 		fs.readdir('./data', function(error, filelist){
 			fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-				const title = queryData.id;
-				const list = templateList(filelist);
-				const template = templateHTML(title, list,
+				var title = queryData.id;
+				var list = templateList(filelist);
+				var template = templateHTML(title, list,
 					`
             <form action="/update_process" method="post">
               <input type="hidden" name="id" value="${title}">
@@ -124,16 +129,29 @@ var app = http.createServer(function(request,response){
 			body = body + data;
 		});
 		request.on('end', function(){
-			const post = qs.parse(body);
-			const id = post.id;
-			const title = post.title;
-			const description = post.description;
+			var post = qs.parse(body);
+			var id = post.id;
+			var title = post.title;
+			var description = post.description;
 			fs.rename(`data/${id}`, `data/${title}`, function(error){
 				fs.writeFile(`data/${title}`, description, 'utf8', function(err){
 					response.writeHead(302, {Location: `/?id=${title}`});
 					response.end();
 				})
 			});
+		});
+	} else if(pathname === '/delete_process'){
+		var body = '';
+		request.on('data', function(data){
+			body = body + data;
+		});
+		request.on('end', function(){
+			var post = qs.parse(body);
+			var id = post.id;
+			fs.unlink(`data/${id}`, function(error){
+				response.writeHead(302, {Location: `/`});
+				response.end();
+			})
 		});
 	} else {
 		response.writeHead(404);
